b632bfafc7a71b91fc6f6f00755a3a24fa132eb5
wssds Sn Gk <ssean.gerigk@gmx.net>
2022-01-10 15:47:27 +0100

    created a slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'seangerigk';
+export class AppComponent implements OnInit {
+  images = ['macbook.dark.jpg', 'shanghai.modern.jpg', 'robotarm.jpg', 'matrix.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
 
 
+  updateImage() {
 
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+
+}
 
 
   
